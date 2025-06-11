5380381f3801ff3783b3e4d7c4a90678c805c875
Ocean <ocean_520x@bytespark.app>
2025-06-11 15:56:15 +0800

    feat：安装tailwind/forms依赖、封装TextInput组件、工作列表页新增查询表单

--- resources/views/job/show.blade.php
+++ resources/views/job/show.blade.php
@@ -1,4 +1,6 @@
 <x-layout>
     <x-bread-crumbs :links="['Jobs' => route('jobs.index'), $job->title => '#']" class="mb-4" />
-    <x-job-card :$job />
+    <x-job-card :$job>
+        <p class="mb-4 text-sm text-slate-500">{!! nl2br(e($job->description)) !!}</p>
+    </x-job-card>
 </x-layout>
