3de8bf1f9ae3b293b445b0b8b6119d9df90b5521
Ocean <ocean_520x@bytespark.app>
2025-06-12 14:23:35 +0800

    feat：输入框清除功能

--- resources/views/job/index.blade.php
+++ resources/views/job/index.blade.php
@@ -1,17 +1,20 @@
 <x-layout>
     <x-bread-crumbs :links="['Jobs' => route('jobs.index')]" class="mb-4" />
     <x-card class="mb-4 text-sm">
-        <form action="{{ route('jobs.index') }}" method="GET">
+        <form id="filtering-form" action="{{ route('jobs.index') }}" method="GET">
             <div class="mb-4 grid grid-cols-2 gap-4">
                 <div>
                     <div class="mb-1 font-semibold">Search</div>
-                    <x-text-input name="search" value="{{ request('search') }}" placeholder="Search for any text" />
+                    <x-text-input name="search" value="{{ request('search') }}" placeholder="Search for any text"
+                        formId="filtering-form" />
                 </div>
                 <div>
                     <div class="mb-1 font-semibold">Salary</div>
                     <div class="flex space-x-2">
-                        <x-text-input name="min_salary" value="{{ request('min_salary') }}" placeholder="From" />
-                        <x-text-input name="max_salary" value="{{ request('max_salary') }}" placeholder="To" />
+                        <x-text-input name="min_salary" value="{{ request('min_salary') }}" placeholder="From"
+                            formId="filtering-form" />
+                        <x-text-input name="max_salary" value="{{ request('max_salary') }}" placeholder="To"
+                            formId="filtering-form" />
                     </div>
                 </div>
                 <div>
