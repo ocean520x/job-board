1eb3307c217972ed2096b8441343afbe5d7f6804
Ocean <ocean_520x@bytespark.app>
2025-06-17 15:19:31 +0800

    feat：安装Alpine依赖、修复x-radio-group组件勾选bug

--- resources/views/components/radio-group.blade.php
+++ resources/views/components/radio-group.blade.php
@@ -6,7 +6,7 @@
 
     @foreach ($optionsWithLabels as $label => $option)
         <label for="{{ $name }}" class="mb-1 flex items-center">
-            <input type="radio" name="{{ $name }}" value="{{ $option }}" @checked($option === request('experience')) />
+            <input type="radio" name="{{ $name }}" value="{{ $option }}" @checked($option === request($name)) />
             <span class="ml-2">{{ $label }}</span>
         </label>
     @endforeach
