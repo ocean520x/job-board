82e6619d6738aef82b1945e7de451407d83493ad
Ocean <ocean_520x@bytespark.app>
2025-06-18 16:29:17 +0800

    feat：Auth控制器、登陆前端页面布局、Auth路由注册

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,19 +2,16 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Job;
 use Illuminate\Http\Request;
 
-class JobController extends Controller
+class AuthController extends Controller
 {
     /**
      * Display a listing of the resource.
      */
     public function index()
     {
-        $filters = request()->only('search', 'min_salary', 'max_salary', 'experience', 'category');
-
-        return view('job.index', ['jobs' => Job::with('employer')->filter($filters)->get()]);
+        //
     }
 
     /**
@@ -22,7 +19,7 @@ class JobController extends Controller
      */
     public function create()
     {
-        //
+        return view('auth.create');
     }
 
     /**
@@ -36,9 +33,9 @@ class JobController extends Controller
     /**
      * Display the specified resource.
      */
-    public function show(Job $job)
+    public function show(string $id)
     {
-        return view('job.show', ['job' => $job->load('employer.jobs')]);
+        //
     }
 
     /**
--- resources/views/components/text-input.blade.php
+++ resources/views/components/text-input.blade.php
@@ -9,7 +9,7 @@
             </svg>
         </button>
     @endif
-    <input x-ref="input-{{ $name }}" type="text" placeholder="{{ $placeholder }}" name="{{ $name }}"
+    <input x-ref="input-{{ $name }}" type="{{$type}}" placeholder="{{ $placeholder }}" name="{{ $name }}"
         value="{{ $value }}" id="{{ $name }}"
         class="pr-8 w-full rounded-md border-0 py-1.5 px-2.5 text-sm ring-1 ring-slate-300 placeholder:text-slate-400 focus:outline-none focus:ring-2" />
 </div>
