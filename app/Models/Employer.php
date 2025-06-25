20fffec62be0bb80736e2f75883391c06590238b
Ocean <ocean_520x@bytespark.app>
2025-06-25 16:37:02 +0800

    feat：用户创建一对一employer

--- app/Models/Employer.php
+++ app/Models/Employer.php
@@ -12,6 +12,11 @@ class Employer extends Model
     /** @use HasFactory<\Database\Factories\EmployersFactory> */
     use HasFactory;
 
+
+    protected $fillable = [
+        'company_name',
+    ];
+
     public function jobs(): HasMany
     {
         return $this->hasMany(Job::class);
